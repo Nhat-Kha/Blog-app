eb17982dd548f153324170877cc9c007bdd908a7
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-04-10 12:28:00 +0700

    create func update, delete, get user, get all user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,2 +1,142 @@
 const bcryptjs = require("bcryptjs");
+const errorHandler = require("../utils/error");
+const User = require("../model/user");
 
+const Test = async (req, res) => {
+  res.json({ message: "API is working" });
+};
+
+const getAllUserOfAdmin = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllUser = async (req, res, next) => {
+  try {
+    const allUser = await User.find();
+
+    res.status(200).json({ allUser, message: "show all user successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "Password must be at least 6 characters"));
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.userName) {
+    if (req.body.userName.length < 7 || req.body.userName.length > 20) {
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
+    }
+    if (req.body.userName.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers")
+      );
+    }
+  }
+  try {
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  if (!req.user.isAdmin && !req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  Test,
+  getUser,
+  getAllUser,
+  getAllUserOfAdmin,
+  updateUser,
+  deleteUser,
+};
