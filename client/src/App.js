3f5c155252e188e981ebc8b1e7c817616cbaaaa1
Kaio-Nguyen <nhatkha06299@gmail.com>
2024-04-11 13:43:49 +0700

    create and update folder redux, components, pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import Header from "./components/Header";
+import PrivateRoute from "./components/ProtectRoute/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/ProtectRoute/OnlyAdminPrivateRoute";
+import FooterCom from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" />
+        <Route path="/sign-up" />
+        <Route path="/search" />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" />
+          <Route path="/update-post/:postId" />
+        </Route>
+        <Route path="/projects" />
+        <Route path="/post/:postSlug" />
+      </Routes>
+      <FooterCom />
+    </BrowserRouter>
   );
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { store, persistor } from "./redux/store.js";
+import ThemeProvider from "./components/ProtectRoute/ThemeProvider";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </PersistGate>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
